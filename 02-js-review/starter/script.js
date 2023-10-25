da1aaaed8fd3f5a845147546a3bd88700db2beed
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-25 16:42:10 +0800

    JS Review: Async JS with Async-Await

--- 02-js-review/starter/script.js
+++ 02-js-review/starter/script.js
@@ -297,10 +297,20 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 console.log(booksAfterUpdate);
 */
 
-// Async JS
+// Async JS (Promise)
 
 fetch("https://jsonplaceholder.typicode.com/todos/1")
 	.then((res) => res.json())
 	.then((data) => console.log(data));
 
 console.log("hello world");
+
+// Async JS (Async/Await)
+
+async function getTodos() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	const data = await res.json();
+	return data;
+}
+
+console.log(await getTodos());
