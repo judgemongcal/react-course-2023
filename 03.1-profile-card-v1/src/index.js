26bac7d83605710e2da28a0da1f07e86cbf61202
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-27 21:27:12 +0800

    created info component

--- 03.1-profile-card-v1/src/index.js
+++ 03.1-profile-card-v1/src/index.js
@@ -5,10 +5,27 @@ import "./index.css";
 function App() {
 	return (
 		<div>
+			<Info />
 			<Skillset />
 		</div>
 	);
 }
+
+const Info = (props) => {
+	return (
+		<div>
+			<h1>Judge Mongcal</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor,
+				lacus a tempus accumsan, quam lorem ullamcorper mauris, sed rhoncus est
+				odio condimentum odio. Nulla a arcu at dui posuere tempus nec quis
+				velit. Nulla pretium quis ante in euismod. Donec sollicitudin porta
+				finibus. Ut mollis lacus nunc, et luctus leo posuere id. Donec euismod
+				pulvinar lobortis. Donec iaculis nibh ut neque mollis imperdiet.{" "}
+			</p>
+		</div>
+	);
+};
 
 const Skillset = (props) => {
 	return (
