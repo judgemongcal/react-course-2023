eab561a93c19828eb54c4be7f827d436c1cc9ad7
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-29 21:10:57 +0800

    more on States

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+	return (
+		<div>
+			<Steps />
+			<Steps />
+		</div>
+	);
+}
+
+function Steps() {
 	const [step, setStep] = useState(1);
 	const [isOpen, setIsOpen] = useState(true);
 
@@ -19,7 +28,7 @@ export default function App() {
 	}
 
 	return (
-		<>
+		<div>
 			<button className="close" onClick={() => setIsOpen((is) => !is)}>
 				&times;
 			</button>
@@ -50,6 +59,6 @@ export default function App() {
 					</div>
 				</div>
 			)}
-		</>
+		</div>
 	);
 }
