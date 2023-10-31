fdcafec08376a17f2ead06953c2077c01f5e8a17
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-31 21:17:11 +0800

    rendering items list

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+	{ id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 export default function App() {
 	return (
 		<div className="app">
@@ -24,10 +30,27 @@ function Form() {
 }
 
 function PackingList() {
-	return <div className="list">LIST</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item item={item} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
-function Item() {}
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
+}
 
 function Stats() {
 	return (
