3815317b19a81690ed51db4d75b4f8bd50bde1ca
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-31 20:47:51 +0800

    Added main components for far away proj

--- 04.2-far-away/src/App.js
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-
-export default function App() {
-	return <h1>Far away</h1>;
-}
---
+++ 05-far-away/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function App() {
+	return (
+		<div className="app">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
+
+function Logo() {
+	return <h1> 🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for your 😍 trip?</h3>
+		</div>
+	);
+}
+
+function PackingList() {
+	return <div className="list">LIST</div>;
+}
+
+function Item() {}
+
+function Stats() {
+	return (
+		<footer className="stats">
+			<em>💼 You have X items on your list, and you already packed X (X%)</em>
+		</footer>
+	);
+}
