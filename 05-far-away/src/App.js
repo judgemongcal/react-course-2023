f214b8bbbc889a2bb8b4dcb0dca0ac96407798c1
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-31 22:37:37 +0800

    Added form functions to fetch input and turn it into an object

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -22,21 +22,39 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(1);
+
 	function handleSubmit(e) {
 		e.preventDefault();
+
+		if (!description) return;
+
+		const newItem = { id: Date.now(), description, quantity, packed: false };
+		console.log(newItem);
+		setDescription("");
+		setQuantity(1);
 	}
 
 	return (
 		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😍 trip?</h3>
-			<select>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}
+			>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type="text" placeholder="Item..." />
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
 			<button>Add</button>
 		</form>
 	);
