eabbb04065b24c42f4b1707513e2eced7ad3b259
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-31 20:20:10 +0800

    added days value connection to step value

--- 04.1-date-counter/src/App.js
+++ 04.1-date-counter/src/App.js
@@ -13,17 +13,17 @@ function Counter() {
 	const [days, daysCnt] = useState(1);
 	return (
 		<div className="flex">
-			<div>
+			<div className="btns">
 				<button onClick={() => stepCnt((step) => step - 1)}>-</button>
 				<span>Step: {step}</span>
 				<button onClick={() => stepCnt((step) => step + 1)}>+</button>
 			</div>
-
-			<div>
-				<button onClick={() => daysCnt((days) => days - 1)}>-</button>
+			<div className="btns">
+				<button onClick={() => daysCnt((days) => days - step)}>-</button>
 				<span>Count: {days}</span>
-				<button onClick={() => daysCnt((days) => days + 1)}>+</button>
+				<button onClick={() => daysCnt((days) => days + step)}>+</button>
 			</div>
+			<div className="main-text">{days} from today is Wed Jul 21, 2027.</div>
 		</div>
 	);
 }
