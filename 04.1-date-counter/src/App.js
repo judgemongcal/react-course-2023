dce0a155417089734f516abc5fc62e965bf752b1
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-02 08:25:45 +0800

    date counter v2

--- 04.1-date-counter/src/App.js
+++ 04.1-date-counter/src/App.js
@@ -10,19 +10,37 @@ export default function App() {
 
 function Counter() {
 	const [step, stepCnt] = useState(1);
-	const [days, daysCnt] = useState(1);
+	const [days, daysCnt] = useState(0);
 	const date = new Date();
 	date.setDate(date.getDate() + days);
+
+	function handleReset() {
+		daysCnt(0);
+		stepCnt(1);
+	}
+
 	return (
 		<div className="flex">
 			<div className="btns">
-				<button onClick={() => stepCnt((step) => step - 1)}>-</button>
+				<input
+					type="range"
+					min="0"
+					max="10"
+					value={step}
+					onChange={(e) => stepCnt(Number(e.target.value))}
+				/>
+				{/* <button onClick={() => stepCnt((step) => step - 1)}>-</button> */}
 				<span>Step: {step}</span>
-				<button onClick={() => stepCnt((step) => step + 1)}>+</button>
+				{/* <button onClick={() => stepCnt((step) => step + 1)}>+</button> */}
 			</div>
 			<div className="btns">
 				<button onClick={() => daysCnt((days) => days - step)}>-</button>
-				<span>Count: {days}</span>
+				{/* <span>Count: {days}</span> */}
+				<input
+					type="text"
+					value={days}
+					onChange={(e) => daysCnt(Number(e.target.value))}
+				/>
 				<button onClick={() => daysCnt((days) => days + step)}>+</button>
 			</div>
 			<div className="main-text">
@@ -30,9 +48,14 @@ function Counter() {
 					? "Today is "
 					: days < 0
 					? `${Math.abs(days)} days ago is `
-					: `${days} from today is `}{" "}
+					: `${Math.abs(days)} day from today is `}{" "}
 				Wed {date.toDateString()}.
 			</div>
+			{step !== 1 || days !== 0 ? (
+				<button className="btns" onClick={handleReset}>
+					Reset
+				</button>
+			) : null}
 		</div>
 	);
 }
