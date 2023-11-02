713fcaba50d2499ec57ef6ab0539d77068867bc0
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-02 08:50:19 +0800

    lifting up state to global (far away proj)

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+	const [items, setItems] = useState([]);
+
+	function handleAddItems(item) {
+		setItems((i) => [...items, item]);
+	}
+
 	return (
 		<div className="app">
 			<Logo />
-			<Form />
-			<PackingList />
+			<Form onAddItems={handleAddItems} />
+			<PackingList items={items} />
 			<Stats />
 		</div>
 	);
@@ -21,9 +27,10 @@ function Logo() {
 	return <h1> 🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
 	const [description, setDescription] = useState("");
 	const [quantity, setQuantity] = useState(1);
+	const [items, setItems] = useState([]);
 
 	function handleSubmit(e) {
 		e.preventDefault();
@@ -31,7 +38,8 @@ function Form() {
 		if (!description) return;
 
 		const newItem = { id: Date.now(), description, quantity, packed: false };
-		console.log(newItem);
+		onAddItems(newItem);
+
 		setDescription("");
 		setQuantity(1);
 	}
@@ -60,11 +68,11 @@ function Form() {
 	);
 }
 
-function PackingList() {
+function PackingList({ items }) {
 	return (
 		<div className="list">
 			<ul>
-				{initialItems.map((item) => (
+				{items.map((item) => (
 					<Item item={item} key={item.id} />
 				))}
 			</ul>
