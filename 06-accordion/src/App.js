d1949f34a75ceb0dc78cfa8be3d7486533d58e28
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-04 08:01:59 +0800

    refactored accordion mini-proj to have children props

--- 06-accordion/src/App.js
+++ 06-accordion/src/App.js
@@ -25,32 +25,35 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+	const [currOpen, setCurrOpen] = useState(null);
 	return (
 		<div className="accordion">
 			{data.map((el, index) => (
 				<AccordionItem
 					num={index + 1}
 					title={el.title}
-					text={el.text}
 					key={el.title}
-				/>
+					currOpen={currOpen}
+					onOpen={setCurrOpen}
+				>
+					{el.text}
+				</AccordionItem>
 			))}
 		</div>
 	);
 }
 
-function AccordionItem({ num, title, text }) {
-	const [isOpen, setIsOpen] = useState(false);
-
+function AccordionItem({ num, title, children, currOpen, onOpen }) {
+	const isOpen = num === currOpen;
 	function handleToggle() {
-		setIsOpen((isOpen) => !isOpen);
+		onOpen(isOpen ? null : num);
 	}
 	return (
 		<div className={`item ${isOpen ? `open` : ""}`} onClick={handleToggle}>
 			<p className="number">{num < 9 ? `0${num}` : num}</p>
 			<p className="title">{title}</p>
 			<p className="icon">{isOpen ? "-" : "+"}</p>
-			{isOpen && <div className="content-box">{text}</div>}
+			{isOpen && <div className="content-box">{children}</div>}
 		</div>
 	);
 }
