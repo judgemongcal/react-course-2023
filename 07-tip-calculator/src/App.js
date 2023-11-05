b399ae2dc2cbe39b187657ec39b25bd88334102d
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-05 15:19:14 +0800

    added Bill component

--- 07-tip-calculator/src/App.js
+++ 07-tip-calculator/src/App.js
@@ -2,5 +2,24 @@ import "./index.css";
 import { useState } from "react";
 
 export default function App() {
-	return <Bill />;
+	return (
+		<div className="container">
+			<Bill />
+		</div>
+	);
 }
+
+const Bill = () => {
+	const [bill, setBill] = useState(0);
+	return (
+		<div className="comp">
+			<span>How much was the bill?</span>
+			<input
+				type="text"
+				placeholder="Enter price of bill.."
+				value={bill}
+				onChange={(e) => setBill(Number(e.target.value))}
+			/>
+		</div>
+	);
+};
