39529190d23ca0514b8874e0333f464eb7877121
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-05 18:14:47 +0800

    fetched input from add friend form & added some states and function to handle onChange

--- 08-eat-n-split/src/App.js
+++ 08-eat-n-split/src/App.js
@@ -91,12 +91,43 @@ const Friend = ({ friend }) => {
 };
 
 const FormAddFriend = () => {
+	const [name, setName] = useState("");
+	const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+	function handleSubmit(e) {
+		if (!name || !image) return;
+
+		e.preventDefault();
+		const id = crypto.randomUUID();
+		const newFriend = {
+			name,
+			image: `${image}?=${id}`,
+			balance: 0,
+			id,
+		};
+
+		setName("");
+		setImage("https://i.pravatar.cc/48");
+	}
+
 	return (
-		<form className="form-add-friend">
+		<form className="form-add-friend" onSubmit={handleSubmit}>
 			<label>👫🏻 Friend name</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => {
+					setName(e.target.value);
+				}}
+			/>
 			<label>🎇 Image URL</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={image}
+				onChange={(e) => {
+					setImage(e.target.value);
+				}}
+			/>
 
 			<Button>Add</Button>
 		</form>
