036f7ed53c8aa0486a8184274f0472a4aad44607
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-05 16:40:37 +0800

    built the static layout of split bill form

--- 08-eat-n-split/src/App.js
+++ 08-eat-n-split/src/App.js
@@ -30,6 +30,7 @@ export default function App() {
 				<FormAddFriend />
 				<Button>Add Friend</Button>
 			</div>
+			<FormSplitBill />
 		</div>
 	);
 }
@@ -86,6 +87,31 @@ const FormAddFriend = () => {
 			<input type="text" />
 
 			<Button>Add</Button>
+		</form>
+	);
+};
+
+const FormSplitBill = () => {
+	return (
+		<form className="form-split-bill">
+			<h2>Split a bill with X</h2>
+
+			<label>💵 Bill Value</label>
+			<input type="text" />
+
+			<label>🤑 Your Expense</label>
+			<input type="text" />
+
+			<label>😎 X's Expense</label>
+			<input type="text" disabled />
+
+			<label>🤑 Who is paying the Bill</label>
+			<select>
+				<option value="user">You</option>
+				<option value="friend">X</option>
+			</select>
+
+			<Button>Split Bill</Button>
 		</form>
 	);
 };
