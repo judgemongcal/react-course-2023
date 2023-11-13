9fdfe06d8979e5ec031ff48f9409fc228c231f4b
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-13 14:31:50 +0800

    added controlled elements, useEffects, and fetch function

--- 12-currency-converter/src/App.js
+++ 12-currency-converter/src/App.js
@@ -1,25 +1,63 @@
 import "./index.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+	const [value, setValue] = useState(0);
+	const [startCur, setStartCur] = useState("USD");
+	const [resultCur, setResultCur] = useState("");
+	const [convertedVal, setConvertedVal] = useState("");
+	useEffect(
+		function () {
+			async function fetchConversion() {
+				if (value && startCur && resultCur) {
+					const res = await fetch(
+						`https://api.frankfurter.app/latest?amount=${value}&from=${startCur}&to=${resultCur}`,
+					);
+					const data = await res.json();
+					setConvertedVal(data.rates[resultCur]);
+				}
+			}
+			fetchConversion();
+
+			// return function () {
+			// 	setValue(1);
+			// 	setStartCur("USD");
+			// 	setResultCur("");
+			// };
+		},
+		[value, startCur, resultCur],
+	);
+
 	return (
 		<div>
-			<input type="text" />
-			<select>
+			<input
+				type="text"
+				value={value}
+				onChange={(e) => setValue(Number(e.target.value))}
+			/>
+			<select
+				value={startCur}
+				onChange={(inCurr) => setStartCur(inCurr.target.value)}
+			>
 				<option value="USD">USD</option>
 				<option value="EUR">EUR</option>
 				<option value="CAD">CAD</option>
 				<option value="INR">INR</option>
 			</select>
-			<select>
+			<select
+				value={resultCur}
+				onChange={(resCurr) => setResultCur(resCurr.target.value)}
+			>
 				<option value="USD">USD</option>
 				<option value="EUR">EUR</option>
 				<option value="CAD">CAD</option>
 				<option value="INR">INR</option>
 			</select>
-			<p>OUTPUT</p>
+			<p>
+				{value < 1 ? "0" : convertedVal} {resultCur}
+			</p>
 		</div>
 	);
 }
