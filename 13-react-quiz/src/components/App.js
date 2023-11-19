9b4aab0392c3c4099f7873278d83b5ac65512544
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-19 15:30:13 +0800

    added Timer component

--- 13-react-quiz/src/components/App.js
+++ 13-react-quiz/src/components/App.js
@@ -11,6 +11,9 @@ import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
 	questions: [],
@@ -19,6 +22,7 @@ const initialState = {
 	answer: null,
 	points: 0,
 	highscore: 0,
+	secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -28,7 +32,11 @@ function reducer(state, action) {
 		case "dataFailed":
 			return { ...state, questions: action.payload, status: "error" };
 		case "start":
-			return { ...state, status: "active" };
+			return {
+				...state,
+				status: "active",
+				secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+			};
 		case "newAnswer":
 			const question = state.questions.at(state.index);
 
@@ -51,14 +59,22 @@ function reducer(state, action) {
 			};
 		case "restart":
 			return { ...initialState, questions: state.questions, status: "ready" };
+		case "tick":
+			return {
+				...state,
+				secondsRemaining: state.secondsRemaining - 1,
+				status: state.secondsRemaining === 0 ? "finished" : state.status,
+			};
 		default:
 			throw new Error("Action Unknown.");
 	}
 }
 
 export default function App() {
-	const [{ questions, status, index, answer, points, highscore }, dispatch] =
-		useReducer(reducer, initialState);
+	const [
+		{ questions, status, index, answer, points, highscore, secondsRemaining },
+		dispatch,
+	] = useReducer(reducer, initialState);
 
 	const numQuestions = questions.length;
 	const maxPossiblePoints = questions.reduce(
@@ -97,7 +113,7 @@ export default function App() {
 							answer={answer}
 						/>
 						<Footer>
-							<Timer />
+							<Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
 							<NextButton
 								dispatch={dispatch}
 								answer={answer}
