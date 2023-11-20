bc4f8d163f4558f08ea0225d38f99795981f91da
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-20 16:45:31 +0800

    importing global css file and using :global()

--- 15-worldwise/src/components/AppNav.jsx
+++ 15-worldwise/src/components/AppNav.jsx
@@ -1,4 +1,4 @@
-import { styles } from "../components/AppNav.module.css";
+import styles from "../components/AppNav.module.css";
 function AppNav() {
 	return <nav className={styles.nav}>App Navigation</nav>;
 }
