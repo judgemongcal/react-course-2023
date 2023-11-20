abefeca8d20df38cbc0dc8428ddf1b5ee7bc7cd4
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-20 16:01:34 +0800

    added routes for diff pages

--- 15-worldwise/src/App.jsx
+++ 15-worldwise/src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-	return <div>Worldwise</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="/" element={<Homepage />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
 export default App;
