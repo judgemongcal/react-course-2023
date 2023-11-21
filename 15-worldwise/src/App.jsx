4f834042bfa3950de2027a4b241c5bfb29f47b47
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-21 16:23:04 +0800

    implemented countries list

--- 15-worldwise/src/App.jsx
+++ 15-worldwise/src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -44,7 +45,10 @@ function App() {
 							path="cities"
 							element={<CityList cities={cities} isLoading={isLoading} />}
 						/>
-						<Route path="countries" element={<p>Countries</p>} />
+						<Route
+							path="countries"
+							element={<CountryList cities={cities} isLoading={isLoading} />}
+						/>
 
 						<Route path="form" element={<p>Form</p>} />
 					</Route>
