960e7b997497eaebdc9c079b5b7eddb3df860551
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-22 13:38:14 +0800

    dynamic routes with URL params

--- 15-worldwise/src/App.jsx
+++ 15-worldwise/src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -45,6 +46,7 @@ function App() {
 							path="cities"
 							element={<CityList cities={cities} isLoading={isLoading} />}
 						/>
+						<Route path="cities/:id" element={<City />} />
 						<Route
 							path="countries"
 							element={<CountryList cities={cities} isLoading={isLoading} />}
--- 15-worldwise/src/components/CityItem.jsx
+++ 15-worldwise/src/components/CityItem.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
@@ -9,13 +10,15 @@ const formatDate = (date) =>
 	}).format(new Date(date));
 
 function CityItem({ city }) {
-	const { cityName, emoji, date } = city;
+	const { cityName, emoji, date, id } = city;
 	return (
-		<li className={styles.cityItem}>
-			<span className={emoji}>{emoji}</span>
-			<h3 className={styles.name}>{cityName}</h3>
-			<time className={styles.date}>{formatDate(date)}</time>
-			<button className={styles.deleteBtn}>&times;</button>
+		<li>
+			<Link className={styles.cityItem} to={`${id}`}>
+				<span className={emoji}>{emoji}</span>
+				<h3 className={styles.name}>{cityName}</h3>
+				<time className={styles.date}>{formatDate(date)}</time>
+				<button className={styles.deleteBtn}>&times;</button>
+			</Link>
 		</li>
 	);
 }
