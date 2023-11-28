057e7876d48d1291826dd85a7c9bb0b5b9a80abc
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-28 18:02:27 +0800

    created FakeAuthContext.jsx for Fake Auth feature

---
+++ 15-worldwise/src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+	return <AuthContext.Provider>{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+	const context = useContext(AuthContext);
+	if (context === undefined)
+		throw new Error("AuthContext was used outside the AuthProvider");
+}
