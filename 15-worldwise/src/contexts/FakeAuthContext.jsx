3836316e00ffbd53bb1786fda79772cfb03ee087
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-28 18:04:44 +0800

    added reducer to handle login and user states

--- 15-worldwise/src/contexts/FakeAuthContext.jsx
+++ 15-worldwise/src/contexts/FakeAuthContext.jsx
@@ -1,8 +1,26 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+	user: null,
+	isAuthenticated: false,
+};
+
+function reducer(state, action) {
+	switch (action) {
+	}
+}
+
 function AuthProvider({ children }) {
+	const [{ user, isAuthenticated }, dispatch] = useReducer(
+		reducer,
+		initialState,
+	);
+
+	function login(email, password) {}
+
+	function logout() {}
 	return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
