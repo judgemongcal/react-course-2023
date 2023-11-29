66ee241f390473dcbeff841843b1bcdde1e152e9
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-29 21:47:01 +0800

    imported isAuthenticated function and created useEffect to navigate to app if authenticated

--- 15-worldwise/src/pages/Login.jsx
+++ 15-worldwise/src/pages/Login.jsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/FakeAuthContext";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
 	// PRE-FILL FOR DEV PURPOSES
 	const [email, setEmail] = useState("jack@example.com");
 	const [password, setPassword] = useState("qwerty");
 
-	const { login } = useAuth();
+	const { login, isAuthenticated } = useAuth();
+	const navigate = useNavigate();
 
 	function handleSubmit(e) {
 		e.preventDefault();
 
 		if (email && password) login(email, password);
 	}
+
+	useEffect(
+		function () {
+			if (isAuthenticated) navigate("/app");
+		},
+		[isAuthenticated, navigate],
+	);
 
 	return (
 		<>
