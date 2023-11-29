341943934013b4ded58f6fcb19a02728de54b946
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-29 21:37:58 +0800

    adeed AuthProvided into the whole component tree

--- 15-worldwise/src/App.jsx
+++ 15-worldwise/src/App.jsx
@@ -11,29 +11,32 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
 	return (
-		<CitiesProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="product" element={<Product />} />
-					<Route path="pricing" element={<Pricing />} />
-					<Route path="app" element={<AppLayout />}>
-						<Route index element={<Navigate replace to="cities" />} />
+		<AuthProvider>
+			<CitiesProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="product" element={<Product />} />
+						<Route path="pricing" element={<Pricing />} />
+						<Route path="app" element={<AppLayout />}>
+							<Route index element={<Navigate replace to="cities" />} />
 
-						<Route path="cities" element={<CityList />} />
-						<Route path="cities/:id" element={<City />} />
-						<Route path="countries" element={<CountryList />} />
+							<Route path="cities" element={<CityList />} />
+							<Route path="cities/:id" element={<City />} />
+							<Route path="countries" element={<CountryList />} />
 
-						<Route path="form" element={<Form />} />
-					</Route>
-					<Route index element={<Homepage />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</CitiesProvider>
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route index element={<Homepage />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
 	);
 }
 export default App;
